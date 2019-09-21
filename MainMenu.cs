2154ca83c033489c5de90b09886bb51735108e77
DevramKorda <32836992+DevramKorda@users.noreply.github.com>
2019-09-21 22:55:40 +0300

    Add files via upload

---
+++ MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour {
+
+	void Start () {		
+	}
+	
+
+	void Update () {		
+	}
+
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene("Level_Select");
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
